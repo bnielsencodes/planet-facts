a92cd45053b34237a0431c0967a22bdc9fc5acc6
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2022-08-20 21:05:01 -0700

    Add js variables for active styling

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// SMALL SCREEN VARIABLES
+const toggleMenuSmItemOne = document.querySelector(".toggle-menu-sm__item--one");
+const toggleMenuSmItemTwo = document.querySelector(".toggle-menu-sm__item--two");
+const toggleMenuSmItemThree = document.querySelector(".toggle-menu-sm__item--three");
+
+// MEDIUM AND LARGE SCREEN VARIABLES
+const toggleMenuItemOne = document.querySelector(".toggle-menu__item--one");
+const toggleMenuItemTwo = document.querySelector(".toggle-menu__item--two");
+const toggleMenuItemThree = document.querySelector(".toggle-menu__item--three");
+
+// PLANET IMAGE VARIABLES
+const planetPrimary = document.querySelector(".planet__img--primary");
+const planetSecondary = document.querySelector(".planet__img--secondary");
+const planetTernary = document.querySelector(".planet__ternary");
+
+// INFO TEXT VARIABLES
+const infoTextPrimary = document.querySelector(".info__text--primary");
+const infoTextSecondary = document.querySelector(".info__text--secondary");
+const infoTextTernary = document.querySelector(".info__text--ternary");
+
