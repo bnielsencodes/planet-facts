cf1f634e34f97bba08aedd76bb8a3f2c900bd697
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2022-08-20 21:05:38 -0700

    Add event listeners for small screen active styling

--- js/app.js
+++ js/app.js
@@ -18,3 +18,42 @@ const infoTextPrimary = document.querySelector(".info__text--primary");
 const infoTextSecondary = document.querySelector(".info__text--secondary");
 const infoTextTernary = document.querySelector(".info__text--ternary");
 
+// SMALL SCREEN OVERVIEW EVENT LISTENER
+toggleMenuSmItemOne.addEventListener("click", () => {
+  planetPrimary.classList.remove("hidden");
+  planetSecondary.classList.add("hidden");
+  planetTernary.classList.add("hidden");
+  infoTextPrimary.classList.remove("hidden");
+  infoTextSecondary.classList.add("hidden");
+  infoTextTernary.classList.add("hidden");
+  toggleMenuSmItemOne.classList.add("toggle-menu-sm__item--active");
+  toggleMenuSmItemTwo.classList.remove("toggle-menu-sm__item--active");
+  toggleMenuSmItemThree.classList.remove("toggle-menu-sm__item--active");
+});
+
+// SMALL SCREEN STRUCTURE EVENT LISTENER
+toggleMenuSmItemTwo.addEventListener("click", () => {
+  planetPrimary.classList.add("hidden");
+  planetSecondary.classList.remove("hidden");
+  planetTernary.classList.add("hidden");
+  infoTextPrimary.classList.add("hidden");
+  infoTextSecondary.classList.remove("hidden");
+  infoTextTernary.classList.add("hidden");
+  toggleMenuSmItemOne.classList.remove("toggle-menu-sm__item--active");
+  toggleMenuSmItemTwo.classList.add("toggle-menu-sm__item--active");
+  toggleMenuSmItemThree.classList.remove("toggle-menu-sm__item--active");
+});
+
+// SMALL SCREEN SURFACE EVENT LISTENER
+toggleMenuSmItemThree.addEventListener("click", () => {
+  planetPrimary.classList.remove("hidden");
+  planetSecondary.classList.add("hidden");
+  planetTernary.classList.remove("hidden");
+  infoTextPrimary.classList.add("hidden");
+  infoTextSecondary.classList.add("hidden");
+  infoTextTernary.classList.remove("hidden");
+  toggleMenuSmItemOne.classList.remove("toggle-menu-sm__item--active");
+  toggleMenuSmItemTwo.classList.remove("toggle-menu-sm__item--active");
+  toggleMenuSmItemThree.classList.add("toggle-menu-sm__item--active");
+});
+
